471e9243a123a2ac81d9c37783c323f0329e69df
Kerwin Tang <kerwintang@gmail.com>
2012-10-04 23:30:19 +0800

    Initial Commit

---
+++ DivineLeeTests/DivineLeeTests.h
@@ -0,0 +1,13 @@
+//
+//  DivineLeeTests.h
+//  DivineLeeTests
+//
+//  Created by Ana Katrina  Chong on 10/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DivineLeeTests : SenTestCase
+
+@end
---
+++ DivineLeeTests/DivineLeeTests.m
@@ -0,0 +1,32 @@
+//
+//  DivineLeeTests.m
+//  DivineLeeTests
+//
+//  Created by Ana Katrina  Chong on 10/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "DivineLeeTests.h"
+
+@implementation DivineLeeTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DivineLeeTests");
+}
+
+@end
